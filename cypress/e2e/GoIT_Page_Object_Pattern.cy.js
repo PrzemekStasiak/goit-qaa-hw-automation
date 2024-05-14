62f4d0c46e9fdbb20b5d461e6fb5971e47f8056f
PrzemekStasiak <140719984+PrzemekStasiak@users.noreply.github.com>
2024-05-14 20:03:16 +0200

    ..

--- cypress/e2e/GoIT_Page_Object_Pattern.cy.js
+++ cypress/e2e/GoIT_Page_Object_Pattern.cy.js
@@ -4,7 +4,7 @@ import { HomePage } from "../pages/HomePage";//import HomePage.js
 const loginPage = new Login(); //constructor of ne class Login
 const homePage = new HomePage();///constructor of ne class HomePage
 
-const testSite = "https://www.edu.goit.global/account/login" // constnt of page to be tested
+const testSite = "https://www.edu.goit.global/account/login"; // constnt of page to be tested
 
 const testEmail1 = "user888@gmail.com";  //test data 1
 const testPass1 = "1234567890"; //test data 1
@@ -14,7 +14,7 @@ const testPass2 = "QA!automation-1";//test data 2
 
 describe("Testing the login page", () => {
   beforeEach(() => {
-    loginPage.visit(testSite); // before every test eneter site using external class to visit tets page
+    loginPage.visit(testSite); // before every test enter site using external class to visit test page
   });
   afterEach(() => {
     homePage.logout(); //after every test logout using external data/class to logout to homepage
