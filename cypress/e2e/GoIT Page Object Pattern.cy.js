b1a6e29f6a2eeb9f49e0bea62e5323d1a72a8939
PrzemekStasiak <140719984+PrzemekStasiak@users.noreply.github.com>
2024-05-14 16:12:54 +0200

    home work page pattern done

---
+++ cypress/e2e/GoIT Page Object Pattern.cy.js
@@ -0,0 +1,29 @@
+import { Login } from "../pages/Login"; // import Login.js 
+import { HomePage } from "../pages/HomePage";//import HomePage.js
+
+const loginPage = new Login(); //constructor of ne class Login
+const homePage = new HomePage();///constructor of ne class HomePage
+
+const testSite = "https://www.edu.goit.global/account/login" // constnt of page to be tested
+
+const testEmail1 = "user888@gmail.com";  //test data 1
+const testPass1 = "1234567890"; //test data 1
+
+const testEmail2 = "testowyqa@qa.team";//test data 2
+const testPass2 = "QA!automation-1";//test data 2
+
+describe('Testing the login page', () =>{
+    it ('test 1: shold login and logout with test data 1',() =>{
+        loginPage.visit(testSite);// using external class to visit tets page
+        loginPage.login (testEmail1, testPass1);//using external class login with test data 1
+        homePage.logout();//using external data to logout to homepage
+    })
+}) 
+
+describe('Testing the login page', () =>{
+    it ('test 2: shold login and logout with test data 2',() =>{
+        loginPage.visit(testSite); // using external class to visit tets page
+        loginPage.login (testEmail2, testPass2);//using external class login with test data 2
+        homePage.logout();//using external data to logout to homepage
+    })
+}) 
---
+++ cypress/pages/Login.js
@@ -0,0 +1,11 @@
+export class Login {  // creating external class to log in in test site
+    visit(testSite) { //visit site
+        cy.visit(testSite)
+    }
+
+    login(email, password) {  //login to site with test data
+        cy.get('#user_email').type(email); //type email in email box
+        cy.get('#user_password').type(password);// type password in password box
+        cy.contains('Log in').click(); //clicl log in button
+    }
+}
